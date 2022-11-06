99911724047efcf00d88fb79763cb84f6ef44a30
wjsyuwls <am2ch1f@gmail.com>
2022-11-06 19:35:05 +0900

    Array

---
+++ inflearn/algorithm_basic/2. Array/4. 피보나치 수열/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public void solution(int n) {
+        int a = 1, b = 1, c;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i < n; i++)
+        {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+
+    /*
+    public int[] solution(int n) {
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+        for (int i = 2; i < n; i++)
+        {
+            answer[i] = answer[i - 2] + answer[i - 1];
+        }
+        return answer;
+    }
+    */
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        T.solution(n);
+    }
+}
